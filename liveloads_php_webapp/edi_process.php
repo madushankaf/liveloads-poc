e99e0f38e7b77e1ac5b4ce86cbcbdc1e5dd98a17
madushankaf <madushankafonseka@live.com>
2023-11-13 13:08:31 +0530

    improve app to add fields

---
+++ liveloads_php_webapp/edi_process.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        foreach ($_POST as $key => $value) {
+            echo $key . ": " . $value . "<br>";
+        }
+    }
+?>
+
+</body>
+</html>
