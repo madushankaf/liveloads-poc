019aa389c2b742e33aca7fcff30232fc9f2ebd06
madushankaf <madushankafonseka@live.com>
2023-11-14 15:24:35 +0530

    included a file upload option

--- liveloads_php_webapp/index.php
+++ liveloads_php_webapp/index.php
@@ -9,8 +9,10 @@
 <h1><?php echo "LiveLoads App"; ?></h1>
 <?php if ($_SERVER["REQUEST_METHOD"] == "GET") { ?>
 
-    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-    <div class="columns">
+    <form method="POST" action="<?php echo $_SERVER[
+        "PHP_SELF"
+    ]; ?>" enctype="multipart/form-data">
+    <!-- <div class="columns">
         <div class="column">
             <label for="ship_notice_date">Ship Notice Date:</label>
             <input type="text" id="ship_notice_date" name="ship_notice_date" value="2023-01-01"><br><br>
@@ -98,94 +100,121 @@
             <input type="text" id="transaction_set_control_number" name="transaction_set_control_number" value="TS-789"><br><br>
         </div>
     </div>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit"> -->
+    Upload an EDI file:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload EDI File" name="submit">
 </form>
 
 
     <?php } ?>
 
-    <?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+        if (
+            isset($_FILES["file"]) &&
+            $_FILES["file"]["error"] == UPLOAD_ERR_OK
+        ) {
+            $uploadedFile = $_FILES["file"]["tmp_name"];
+
+            // Read the content of the file
+            $fileContent = file_get_contents($uploadedFile);
+            echo "<div>";
+            echo "<div>";
+            echo "File Content - EDI String:\n";
+            echo "</div>";
+            echo $fileContent;
+            echo "\n";
+            echo "</div>";
+    
+            $clientId = getenv("CLIENT_ID");
+            $clientSecret = getenv("CLIENT_SECRET");
+            $credentials = base64_encode($clientId . ":" . $clientSecret);
+            $postData = [
+                "grant_type" => "client_credentials",
+            ];
+            $options = [
+                "http" => [
+                    "header" =>
+                        "Content-type: application/x-www-form-urlencoded\r\n" .
+                        "Authorization: Basic " .
+                        $credentials .
+                        "\r\n",
+                    "method" => "POST",
+                    "content" => http_build_query($postData),
+                ],
+            ];
+            $context = stream_context_create($options);
+            $accessToken = file_get_contents(
+                "https://api.asgardeo.io/t/poc4liveloads/oauth2/token",
+                false,
+                $context
+            );
+            $accessToken = json_decode($accessToken)->access_token;
+    
+            // Use the $accessToken as needed
+    
+            $optionsForApi = [
+                "http" => [
+                    "header" =>
+                        "text/plain\r\n" .
+                        "Authorization: Bearer " .
+                        $accessToken .
+                        "\r\n",
+                    "method" => "POST",
+                    "content" => $fileContent,
+                ],
+            ];
+            $contextForApiCall = stream_context_create($optionsForApi);
+            $shippingNoticeSaved = file_get_contents(
+                "https://8d25823b-969a-43a8-84ae-b71755f068b7-dev.e1-us-east-azure.choreoapis.dev/ftyz/liveloads-edi-proxy/endpoint-8090-8b1/v1/ShipNoticeManifest",
+                false,
+                $contextForApiCall
+            );
+    
+            echo "<div>";
+            echo "<div>";
+            echo "Shipping Notice Saved:\n";
+            echo "</div>";
+            echo "<pre>" . json_encode($shippingNoticeSaved, JSON_PRETTY_PRINT) . "</pre>";
+            echo "\n";
+            echo "</div>";
+
+        } else {
+            echo "Error uploading file.";
+        }
         // collect value of input field
         $fields = ""; // Declare the $fields variable
-        $fields .= "ST*856*0001*005010X12~";
-        $fields .= "BSN*".$_POST["ship_notice_date"] . "*";
-        $fields .= $_POST["ship_notice_time"] . "*";
-        $fields .= $_POST["ship_notice_timezone"] . "~";
-        $fields .= "HL*".$_POST["hierarchical_id"] . "*";
-        $fields .= $_POST["hierarchical_parent_id"] . "*";
-        $fields .= $_POST["hierarchical_level_code"] . "~";
-        $fields .= "LIN*".$_POST["product_id"] . "*";
-        $fields .= $_POST["product_description"] . "*";
-        $fields .= $_POST["product_type"] . "*";
-        $fields .= $_POST["product_code"] . "~";
-        $fields .= "SN1*".$_POST["quantity_shipped"] . "*";
-        $fields .= $_POST["packaging_info"] . "*";
-        $fields .= $_POST["weight"] . "*";
-        $fields .= $_POST["weight_unit"] . "~";
-        $fields .= "PID*".$_POST["item_description"] . "*";
-        $fields .= $_POST["characteristic_code"] . "*";
-        $fields .= $_POST["additional_info"] . "*";
-        $fields .= $_POST["info_qualifier"] . "~";
-        $fields .= "N1*".$_POST["entity_identifier_code"] . "*";
-        $fields .= $_POST["name"] . "*";
-        $fields .= $_POST["identification_code"] . "*";
-        $fields .= $_POST["identification_qualifier"] . "~";
-        $fields .= "CTT*".$_POST["number_of_line_items"] . "*";
-        $fields .= $_POST["hash_total"] . "*";
-        $fields .= $_POST["weight_total"] . "*";
-        $fields .= $_POST["weight_unit"] . "~";
-        $fields .= "SE*".$_POST["number_of_included_segments"] . "*";
-        $fields .= $_POST["transaction_set_control_number"] . "~";
+        // $fields .= "ST*856*0001*005010X12~";
+        // $fields .= "BSN*".$_POST["ship_notice_date"] . "*";
+        // $fields .= $_POST["ship_notice_time"] . "*";
+        // $fields .= $_POST["ship_notice_timezone"] . "~";
+        // $fields .= "HL*".$_POST["hierarchical_id"] . "*";
+        // $fields .= $_POST["hierarchical_parent_id"] . "*";
+        // $fields .= $_POST["hierarchical_level_code"] . "~";
+        // $fields .= "LIN*".$_POST["product_id"] . "*";
+        // $fields .= $_POST["product_description"] . "*";
+        // $fields .= $_POST["product_type"] . "*";
+        // $fields .= $_POST["product_code"] . "~";
+        // $fields .= "SN1*".$_POST["quantity_shipped"] . "*";
+        // $fields .= $_POST["packaging_info"] . "*";
+        // $fields .= $_POST["weight"] . "*";
+        // $fields .= $_POST["weight_unit"] . "~";
+        // $fields .= "PID*".$_POST["item_description"] . "*";
+        // $fields .= $_POST["characteristic_code"] . "*";
+        // $fields .= $_POST["additional_info"] . "*";
+        // $fields .= $_POST["info_qualifier"] . "~";
+        // $fields .= "N1*".$_POST["entity_identifier_code"] . "*";
+        // $fields .= $_POST["name"] . "*";
+        // $fields .= $_POST["identification_code"] . "*";
+        // $fields .= $_POST["identification_qualifier"] . "~";
+        // $fields .= "CTT*".$_POST["number_of_line_items"] . "*";
+        // $fields .= $_POST["hash_total"] . "*";
+        // $fields .= $_POST["weight_total"] . "*";
+        // $fields .= $_POST["weight_unit"] . "~";
+        // $fields .= "SE*".$_POST["number_of_included_segments"] . "*";
+        // $fields .= $_POST["transaction_set_control_number"] . "~";
 
-        
-        echo "<div>";
-        echo "<div>";
-        echo "EDI String:\n";
-        echo "</div>";
-        echo $fields;
-        echo "\n";
-        echo "</div>";
 
-        $clientId = getenv("CLIENT_ID");
-        $clientSecret = getenv("CLIENT_SECRET");
-        $credentials = base64_encode($clientId . ":" . $clientSecret);
-        $postData = array(
-            'grant_type' => 'client_credentials'
-        );
-        $options = array(
-            'http' => array(
-                'header' => "Content-type: application/x-www-form-urlencoded\r\n" .
-                            "Authorization: Basic " . $credentials . "\r\n",
-                'method' => 'POST',
-                'content' => http_build_query($postData)
-            )
-        );
-        $context = stream_context_create($options);
-        $accessToken = file_get_contents('https://api.asgardeo.io/t/poc4liveloads/oauth2/token', false, $context);
-        $accessToken = json_decode($accessToken)->access_token;
-
-     
-
-        // Use the $accessToken as needed
-        
-        $optionsForApi = array(
-            'http' => array(
-                'header' => "text/plain\r\n" .
-                            "Authorization: Bearer " . $accessToken . "\r\n",
-                'method' => 'POST',
-                'content' => $fields
-            )
-        );
-        $contextForApiCall = stream_context_create($optionsForApi);
-        $shippingNoticeSaved = file_get_contents('https://8d25823b-969a-43a8-84ae-b71755f068b7-dev.e1-us-east-azure.choreoapis.dev/ftyz/liveloads-edi-proxy/endpoint-8090-8b1/v1/ShipNoticeManifest', false, $contextForApiCall);
-
-        echo "<div>";
-        echo "<div>";
-echo "Shipping Notice Saved:\n";
-echo "</div>";
-echo $shippingNoticeSaved;
-echo "\n";
-echo "</div>";
 
         // Use the $accessToken as needed
     } ?>
